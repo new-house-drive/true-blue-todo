6dc0538bff7c9e459f15692ea8333df42d52226e
valera <lyudfaffnir@gmail.com>
2021-09-09 01:41:36 +0300

    rename to True Blue Todo

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+function trueBlueTodo() {
+    let content = document.getElementById('content')
+
+    title = document.createElement('div')
+    title.className = 'content-title'
+    title.innerText = 'True Blue Todo'
+
+}
+
+
+
+function main() {
+
+}
