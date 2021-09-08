67b58fedf3b83cb0dd67bc25e3b8a6b56d088019
valera <lyudfaffnir@gmail.com>
2021-09-09 01:56:12 +0300

    add poeticEddaQuote

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ function trueBlueTodo() {
     title.className = 'content-title'
     title.innerText = 'True Blue Todo'
 
+
+    /* TODO */
+    /** Background for empty list */
+    // "Cattle die, kinsmen die; the self must also die. I know one thing which never dies: the reputation of each dead man"
+    let poeticEddaQuote = 'Deyr fé,\ndeyja frændr,\ndeyr sjalfr it sama,\nek veit einn,\nat aldrei deyr:\ndómr um dauðan hvern.';
 }
 
 
