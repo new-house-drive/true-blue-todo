c296eeffe805a6384c343d1458381d1f302c8ee1
valera <lyudfaffnir@gmail.com>
2021-09-13 02:36:15 +0300

    extract add top bar func

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-'use strict'
+"use strict";
+import "./css/style.css";
 
+const TrueBlueTodo = (() => {
+  let content = document.getElementById("content");
 
-function trueBlueTodo() {
-    let content = document.getElementById('content')
+  const addTopBar = () => {
+    let title = document.createElement("div");
+    title.className = "content-title";
+    title.innerText = "True Blue Todo";
 
-    title = document.createElement('div')
-    title.className = 'content-title'
-    title.innerText = 'True Blue Todo'
+    content.appendChild(title);
+  };
 
+  /* TODO */
+  /** Background for empty list */
+  // "Cattle die, kinsmen die; the self must also die. I know one thing which never dies: the reputation of each dead man"
+  let poeticEddaQuote =
+    "Deyr fé,\ndeyja frændr,\ndeyr sjalfr it sama,\nek veit einn,\nat aldrei deyr:\ndómr um dauðan hvern.";
 
-    /* TODO */
-    /** Background for empty list */
-    // "Cattle die, kinsmen die; the self must also die. I know one thing which never dies: the reputation of each dead man"
-    let poeticEddaQuote = 'Deyr fé,\ndeyja frændr,\ndeyr sjalfr it sama,\nek veit einn,\nat aldrei deyr:\ndómr um dauðan hvern.';
-}
+  return {
+    addTopBar
+  };
+})()
 
-
-
-function main() {
-
-}
+const Module = (() => {
+  TrueBlueTodo.addTopBar();
+})();
