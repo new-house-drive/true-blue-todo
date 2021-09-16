a35cf6b4e70b9b26197d8d8dc83038b3ade58c2f
valera <lyudfaffnir@gmail.com>
2021-09-16 22:28:14 +0300

    separate code from addTopBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 import "./css/style.css";
-import titleIconPath from './icons/shield.svg'
+import titleIconPath from "./icons/shield.svg";
 
 const TrueBlueTodo = (() => {
   let content = document.getElementById("content");
@@ -10,39 +10,49 @@ const TrueBlueTodo = (() => {
     let topBarContainer = document.createElement("div");
     topBarContainer.className = "top-bar-container";
 
-    /* Text Container */
-    let topBarTextContainer = document.createElement('div')
-    topBarTextContainer.className = 'top-bar-text'
-    let topBarText = document.createElement('h4')
-    topBarText.innerText = "True Blue Todo";
-
-    topBarTextContainer.appendChild(topBarText)
-
-    /* Top bar icon */
-    let topBarIcon = document.createElement('img');
-    topBarIcon.src = titleIconPath;
-    topBarIcon.alt = 'My awesome image'
-    topBarIcon.className = 'top-bar-icon';
-
-    /* Navbar */
-    let navbar = document.createElement('div');
-    navbar.className = 'navbar-container';
-
-    for(let i = 0; i < 3; i++) {
-      let navbarButton = document.createElement('button')
-      navbarButton.className = 'navbar-button'
-      navbarButton.innerText = 'Example button'
-      navbar.appendChild(navbarButton)
-    }
-
-
     /** Append Elements */
-    topBarContainer.appendChild(topBarIcon);
-    topBarContainer.appendChild(topBarTextContainer)
-    topBarContainer.appendChild(navbar);
+    topBarContainer.appendChild(createTopBarIcon());
+    topBarContainer.appendChild(createTopBarTextContainer());
+    topBarContainer.appendChild(createTopBarNavbar());
 
     content.appendChild(topBarContainer);
   };
+
+  const createTopBarTextContainer = () => {
+    let topBarTextContainer = document.createElement("div");
+    topBarTextContainer.className = "top-bar-text";
+    let topBarText = document.createElement("h4");
+    topBarText.innerText = "True Blue Todo";
+
+    topBarTextContainer.appendChild(topBarText);
+    return topBarTextContainer;
+  };
+
+  const createTopBarIcon = () => {
+    /* Top bar icon */
+    let topBarIcon = document.createElement("img");
+    topBarIcon.src = titleIconPath;
+    topBarIcon.alt = "My awesome image";
+    topBarIcon.className = "top-bar-icon";
+
+    return topBarIcon;
+  };
+
+  const createTopBarNavbar = () => {
+    /* Navbar */
+    let navbar = document.createElement("div");
+    navbar.className = "navbar-container";
+
+    for (let i = 0; i < 3; i++) {
+      let navbarButton = document.createElement("button");
+      navbarButton.className = "navbar-button";
+      navbarButton.innerText = "Example button";
+      navbar.appendChild(navbarButton);
+    }
+
+    return navbar;
+  };
+  // Add todos from the list of todos and update them
 
   /* TODO */
   /** Background for empty list */
@@ -51,9 +61,9 @@ const TrueBlueTodo = (() => {
     "Deyr fé,\ndeyja frændr,\ndeyr sjalfr it sama,\nek veit einn,\nat aldrei deyr:\ndómr um dauðan hvern.";
 
   return {
-    addTopBar
+    addTopBar,
   };
-})()
+})();
 
 const Module = (() => {
   TrueBlueTodo.addTopBar();
